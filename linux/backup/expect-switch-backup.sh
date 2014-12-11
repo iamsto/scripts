5c6001544fb21cb72736c9da48d73068ec0a384b
Sto <sto@Stos-MacBook-Pro.local>
2014-12-11 15:43:25 +0200

    Backup Huawey switches to remote FTP server

---
+++ linux/backup/expect-switch-backup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/expect
+set timeout 15
+
+set ftp_name "FTP_NAME"
+set ftp_pass "FTP_PASS"
+set ftp_ip "SW_IP"
+set sw_name "SW_NAME"
+set sw_pass "SW_PASS"
+set sw_ip "SW_IP"
+set sw_user "SW_USERNAME"
+set fname vrpcfg-sw0-[exec date +%Y-%m-%d].zip
+
+spawn ssh $sw_user@$sw_ip -p10022
+
+expect "password:"
+send "$sw_pass\r"
+
+expect "<SW0>"
+send "ftp $ftp_ip\r"
+
+expect "none)):"
+send "$ftp_name\r"
+
+expect "Enter password:"
+send "$ftp_pass\r"
+
+send "binary\r"
+send "cd $sw_name\r"
+
+send "put vrpcfg-$sw_name.zip $fname\r"
+send "quit\r"	
+send "quit\r"
+
+expect eof
